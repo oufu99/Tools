3cffae5efd297b98a5a1106a344cb7ed87980610
51758018@qq.com <51758018@qq.com>
2018-07-26 20:20:45 +0800

    添加 每次新建厂商时需要做的重复工作

---
+++ AddManu/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddManu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //添加厂商  添加一个文件夹 复制需求文档,新建txt,sql
+
+        }
+    }
+}
