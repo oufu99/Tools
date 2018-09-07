a9dab79f7606e1639e683218b59eabf5174cccb0
oufu99 <51758018@qq.com>
2018-09-07 17:21:13 +0800

    a

---
+++ Common/Enums/IISEnums.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public enum IISEnums
+    {
+        WebSitId = 1,
+        MobileSitId = 2
+
+    }
+}
