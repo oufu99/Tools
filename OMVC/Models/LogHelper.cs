f3b8d322dc04734424aa39e1b72ee69996bb7e1f
oufu99 <51758018@qq.com>
2018-09-21 16:11:02 +0800

    ad

--- OMVC/Models/LogHelper.cs
+++ OMVC/Models/LogHelper.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-[assembly: log4net.Config.XmlConfigurator(ConfigFile = @"log4net.config", Watch = true)]
+
 namespace OMVC.Models
 {
     public class LogHelper
