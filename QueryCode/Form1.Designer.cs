6fbc532d3316fa981b93b536dbb32257bdefcbbb
oufu99 <51758018@qq.com>
2018-10-15 09:55:49 +0800

    添加查询密码功能

---
+++ QueryCode/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace QueryCode
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.userTxt = new System.Windows.Forms.TextBox();
+            this.pwdTxt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(545, 118);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "复制admin密码";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(161, 97);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(100, 21);
+            this.txtId.TabIndex = 3;
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(172, 135);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(75, 23);
+            this.btn1.TabIndex = 2;
+            this.btn1.Text = "查询密码";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // userTxt
+            // 
+            this.userTxt.Location = new System.Drawing.Point(161, 242);
+            this.userTxt.Name = "userTxt";
+            this.userTxt.Size = new System.Drawing.Size(191, 21);
+            this.userTxt.TabIndex = 4;
+            // 
+            // pwdTxt
+            // 
+            this.pwdTxt.Location = new System.Drawing.Point(148, 287);
+            this.pwdTxt.Name = "pwdTxt";
+            this.pwdTxt.Size = new System.Drawing.Size(225, 21);
+            this.pwdTxt.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(107, 245);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "账号:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(107, 290);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 12);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "密码:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(545, 175);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(128, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "复制sa密码";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pwdTxt);
+            this.Controls.Add(this.userTxt);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.TextBox userTxt;
+        private System.Windows.Forms.TextBox pwdTxt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ QueryCode/Form1.cs
@@ -0,0 +1,55 @@
+﻿using Common;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QueryCode
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+
+            InitializeComponent();
+            txtId.TabIndex = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var admin = "PT_DSe/XycOhQW_Q8Cu5tIZg_sg";
+            Clipboard.SetText(admin);
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            string manuId = this.txtId.Text.Trim();
+            int outManuId = 0;
+            string sql = "";
+            if (int.TryParse(manuId, out outManuId))
+            {
+                sql = string.Format($@"SELECT b.name,b.password FROM dbo.tb_manufacturer a LEFT JOIN tb_user b ON a.tb_manufacturerID=b.manufacturer_id WHERE a.tb_manufacturerID={manuId} AND b.system_role_id=-10");
+            }
+            else
+            {
+                sql = string.Format($@"SELECT b.name,b.password FROM dbo.tb_manufacturer a LEFT JOIN tb_user b ON a.tb_manufacturerID=b.manufacturer_id WHERE  b.name='{manuId}' AND b.system_role_id=-10");
+            }
+            var model = SQLHelper.Query<tb_manu>(sql);
+            userTxt.Text = model.Name;
+            pwdTxt.Text = model.PassWord;
+            Clipboard.SetText(model.PassWord);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var sa = "WA@@@Wei315#@#WinGG";
+            Clipboard.SetText(sa);
+        }
+    }
+}
