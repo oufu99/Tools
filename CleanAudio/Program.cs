9a163b5e139d39098d1e301adfa110d79466146b
oujialin <oujialin@dossen.com>
2019-12-25 11:20:30 +0800

    aaron commit

--- CleanAudio/Program.cs
+++ CleanAudio/Program.cs
@@ -21,29 +21,46 @@ namespace CleanAudio
             //f.Tag.Album = "认知方法论";
             //f.Save();
 
-            string dirPath = @"E:\认知方法论\音频归类\";
 
-            string targetBasePath = dirPath + @"\temp\";
-            FileHelper.CreateDirectory(targetBasePath);
-            var files = Directory.GetFiles(dirPath);
-            foreach (var item in files)
+
+            //新增外层是父目录
+            var basePath = @"D:\MyLove";
+            var paths = Directory.GetDirectories(basePath).ToList();
+            if (paths.Count() == 0)
             {
-                var path = item.Trim();
-                TagLib.File f = TagLib.File.Create(path);
-
-                var name = Path.GetFileName(path);
-                //网易读取的名字
-                f.Tag.Title = name;
-                //说明的左边
-                f.Tag.Performers = new string[] { name };
-                //说明的右边
-                f.Tag.Album = name;
-
-                f.Tag.Pictures = null;
-                f.Save();
-                var targetPath = targetBasePath + name;
-                FileHelper.MoveFile(item, targetPath);
+                paths.Add(basePath);
             }
+            foreach (var item in paths)
+            {
+                var dirPath = item;
+                if (dirPath[dirPath.Length - 1] != '\\')
+                {
+                    dirPath += @"\";
+                }
+                string targetBasePath = dirPath + @"\temp\";
+                FileHelper.CreateDirectory(targetBasePath);
+                var files = Directory.GetFiles(dirPath);
+                foreach (var file in files)
+                {
+                    var path = file.Trim();
+                    TagLib.File f = TagLib.File.Create(path);
+
+                    var name = Path.GetFileName(path);
+                    //网易读取的名字
+                    f.Tag.Title = name;
+                    //说明的左边
+                    f.Tag.Performers = new string[] { name };
+                    //说明的右边
+                    f.Tag.Album = name;
+
+                    f.Tag.Pictures = null;
+                    f.Save();
+                    var targetPath = targetBasePath + name;
+                    FileHelper.MoveFile(file, targetPath);
+                }
+            }
+
+
 
             Console.WriteLine("清洗完毕");
             Console.ReadLine();
